038e0ee392f5e786ba0f98bd50489498d645485a
Elbert Bae <elbert.bae@gmail.com>
2020-06-15 18:31:38 -0700

    Rename verify user to authenticateUser

---
+++ server/src/controller/user-controller.ts
@@ -0,0 +1,73 @@
+import * as bcrypt from 'bcrypt';
+import * as passport from 'passport';
+import { v4 as uuidv4 } from 'uuid';
+
+import DynamoClient from '../clients/dynamodb';
+import { createUserParams, getUser } from './helpers/user';
+import { omitKeys } from '../utils/object-modifier';
+
+const saltRounds = 8;
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // TODO: Compare hashed bcrypt password for auth
+};
+
+const createUser = async (req, res, next) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  try {
+    await DynamoClient.create(
+      createUserParams({
+        userId: uuidv4(),
+        email,
+        firstName,
+        lastName,
+        password: await bcrypt.hash(password, saltRounds),
+      })
+    );
+
+    console.log(`User created with email: ${email}`);
+
+    res.status(200).send({
+      success: true,
+    });
+  } catch (err) {
+    res.status(500).send({
+      error: err,
+    });
+  }
+};
+
+const authenticateUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await getUser(email);
+    const verified = user && (await bcrypt.compare(password, user.password));
+
+    if (!verified) {
+      res.status(403).send({
+        error: 'Password is incorrect',
+      });
+      return;
+    }
+
+    res.status(200).send({
+      success: true,
+      data: omitKeys(user, ['password']),
+    });
+  } catch (err) {
+    console.error(`Encountered an error fetching user data: ${err}`);
+    res.status(500).send({
+      error: err,
+    });
+  }
+};
+
+export default {
+  login,
+  createUser,
+  authenticateUser,
+};
---
+++ server/src/routes/user-routes.ts
@@ -0,0 +1,14 @@
+import * as express from 'express';
+import UserController from '../controller/user-controller';
+
+const router = express.Router();
+
+router.post('/login', UserController.authenticateUser);
+
+router.post('/create', UserController.createUser);
+
+router.put('/', (req, res, next) => {
+  console.log('/user put endpoint');
+});
+
+export default router;
