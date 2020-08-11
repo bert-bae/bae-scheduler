9df2b8ca0aafc861ce62514a1b1f29e73e6e100b
Elbert Bae <elbert.bae@gmail.com>
2020-08-10 20:37:36 -0700

    Add animation effect to navbar scroll up and down depending on scroll offset

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './navigation-bar.scss';
 
 const NavigationBar = () => {
+  const navRef = useRef<HTMLDivElement>(null);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+    if (currentScrollY > 70) {
+      navRef.current?.setAttribute('data-hidden', 'true');
+    } else {
+      navRef.current?.setAttribute('data-hidden', 'false');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="bae-navbar">
+    <div className="bae-navbar" ref={navRef}>
       <div className="navbar-content">
         <div className="logo-container">
           <img
